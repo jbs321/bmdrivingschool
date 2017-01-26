7b89ef5a4f8155db00a7f14fd7ff9abf885b2207
Jacob <jbs321.work@gmail.com>
2017-01-26 05:45:46 +0000

    sadas

--- wp-content/plugins/motopress-slider-lite/includes/classes/update_fixes/MPSL_Fix.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Interface MPSL_Fix
- * Every class that implements interface must be named as "MPSL_Fix_v{$version}", where $version without dots.
- */
-interface MPSL_Fix {}
--- wp-content/plugins/motopress-slider-lite/views/main.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('ABSPATH')) exit; ?>
-
-<div class="row">
-    <div class="col-md-12">
-        <h4><?php _e('Main Content', 'motopress-slider-lite'); ?></h4>
-    </div>
-</div>
